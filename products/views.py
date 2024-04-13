a90f47b7c828c4337585f1e3a98d42aa07dad4d6
hosseinrezaeian <hossainrezaeian@gmail.com>
2024-04-13 23:12:40 +0330

    connect add prodact to post

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from .models import ProductOrService
+
 
 # Create your views here.
+class ProductView(View):
+    def get(self, request, product_id, *args, **kwargs):
+        product = ProductOrService.objects.get(id=product_id)
+        content = {'product': product}
+        return render(request, 'products/post.html', content)
